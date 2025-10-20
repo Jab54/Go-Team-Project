a8b82e1978ff4b162ceba49cebaf37825a185682
Natalie <hollan62@rowan.edu>
2025-10-20 13:44:27 -0400

    Full functionality: Random, looping, etc.

--- Craps.go
+++ Craps.go
@@ -1,34 +1,43 @@
 package main
 
 import "fmt"
+import "math/rand"
 
 func main() {
 	fmt.Println("From PL Team #1")
 
 	var gameStatus, sum, myPoint int
 
-	sum = rollDice()
+	sum = rollDice() // First roll of the dice
+	//fmt.Println(sum)
 
 	switch sum {
 	case 7:
 		fallthrough
 	case 11:
-		gameStatus = 1
+		gameStatus = 1 // Win on first roll
 	case 2:
 		fallthrough
 	case 3:
 		fallthrough
 	case 12:
-		gameStatus = 2
+		gameStatus = 2 // Lose on first roll
 	default:
-		gameStatus = 0
+		gameStatus = 0 // Remember point
 		myPoint = sum
 		fmt.Printf("Point is: %d\n", myPoint)
-		
 	}
 
-	// While loop commented to avoid infinite loop
-	//while (gameStatus == 0) {}
+	for gameStatus == 0 { // Keep rolling
+		sum = rollDice()
+		//fmt.Println(sum)
+		switch sum {
+		case myPoint: // Win by making point
+			gameStatus = 1
+		case 7: // Lose by rolling 7
+			gameStatus = 2
+		}
+	}
 
 	if gameStatus == 1 {
 		fmt.Println("Player wins")
@@ -39,10 +48,11 @@ func main() {
 
 func rollDice() int {
 	var die1, die2, workSum int
-	// Placeholder values
-	die1 = -1
-	die2 = -1
+	die1 = rand.Intn(6)+1
+	die2 = rand.Intn(6)+1
 	workSum = die1 + die2
+
+	fmt.Printf("Player rolled %d, %d: %d\n", die1, die2, workSum)
 
 	return workSum
 }
